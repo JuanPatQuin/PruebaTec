606cde20fc5d1d1e7c8fbd74790c071244a7e1cf
JuanPatQuin <juanesteban082000@gmail.com>
2024-05-05 19:58:18 -0500

    first commit

---
+++ slider.js
@@ -0,0 +1,20 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(n) {
+    slides[currentSlide].style.display = 'none';
+    currentSlide = (n + slides.length) % slides.length;
+    slides[currentSlide].style.display = 'block';
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+    showSlide(currentSlide - 1);
+}
+
+// Cambiar automáticamente cada 5 segundos
+setInterval(nextSlide, 5000);
+
